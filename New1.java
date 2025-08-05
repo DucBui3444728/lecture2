25554fddea7bd65fd7ab68039d0aac923c901ddf
CUNGVANTHANG <cungvanthang2k3@gmail.com>
2025-08-05 21:01:56 +0700

    push

--- New1.java
+++ New1.java
@@ -1,6 +1,6 @@
 public class New1 {
     public static void main(String[] args) {
-        System.out.println("Chao ban");
+        System.out.println("Modify");
 
         System.out.println("Modify");
     }
